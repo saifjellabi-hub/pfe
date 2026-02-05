af9e807d9bdc1863a75ab1b90565fbdf4571d3f5
saifjellabi <saif.jellabi@gmail.com>
2026-02-05 12:30:01 +0100

    Correction ReactiveFormsModule et test routing ok

--- frontend/src/app/demande-credit/demande-credit.ts
+++ frontend/src/app/demande-credit/demande-credit.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule ,FormGroup, FormControl, Validators} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-demande-credit',
   standalone: true,
-  imports: [CommonModule,RouterModule, FormsModule],
+  imports:[CommonModule,RouterModule,ReactiveFormsModule],
   templateUrl: './demande-credit.html',
   styleUrls: ['./demande-credit.css']
 })
-export class DemandeCredit {}
+export class DemandeCredit {
+
+  creditForm = new FormGroup({
+    montant: new FormControl('', [Validators.required, Validators.min(1000)]),
+    duree: new FormControl('', [Validators.required, Validators.min(6), Validators.max(360)]), // en mois
+    revenuMensuel: new FormControl('', [Validators.required, Validators.min(0)]),
+    profession: new FormControl('', [Validators.required]),
+    objetCredit: new FormControl('immobilier', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.creditForm.valid) {
+      console.log("Données envoyées :", this.creditForm.value);
+      //bch mbaed tekhdem beha el springboot
+      alert("Demande envoyée pour analyse !");
+    } else {
+      alert("Veuillez remplir correctement le formulaire.");
+    }
+  }
+}
--- frontend/src/app/resultat-credit/resultat-credit.ts
+++ frontend/src/app/resultat-credit/resultat-credit.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-resultat-credit',
   standalone: true,
   templateUrl: './resultat-credit.html',
   styleUrl: './resultat-credit.css',
+  imports : [CommonModule,RouterModule]
 })
 export class ResultatCredit {
-
+  score: number = 85; 
+  decision: string = 'Accordé';
 }
